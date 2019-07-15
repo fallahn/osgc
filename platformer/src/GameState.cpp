6f59990896b9592d1b129c776c7d77faa9b0410e
fallahn <matty_styles@hotmail.com>
2019-07-15 20:55:21 +0100

    update assets

--- platformer/src/GameState.cpp
+++ platformer/src/GameState.cpp
@@ -78,7 +78,7 @@ GameState::GameState(xy::StateStack& ss, xy::State::Context ctx, SharedData& sd)
 {
     launchLoadingScreen();
     //sd.theme = "mes";
-    sd.nextMap = "gb02.tmx";
+    //sd.nextMap = "gb02.tmx";
     initScene();
     loadResources();
     buildWorld();
