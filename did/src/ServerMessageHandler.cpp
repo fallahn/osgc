2816b4899217d938f84a0f4ce2fd671836f7d99b
fallahn <matty_styles@hotmail.com>
2019-09-06 15:22:40 +0100

    add missing file

---
+++ did/src/ServerMessageHandler.cpp
@@ -0,0 +1,61 @@
+/*********************************************************************
+
+Copyright 2019 Matt Marchant
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*********************************************************************/
+
+#include "SharedStateData.hpp"
+#include "MessageIDs.hpp"
+
+#include <xyginext/core/App.hpp>
+
+void serverMessageHandler(SharedData& sharedData, const xy::Message& msg)
+{
+    if (msg.id == MessageID::SystemMessage)
+    {
+        const auto& data = msg.getData<SystemEvent>();
+        if (data.action == SystemEvent::RequestStartServer
+            && !sharedData.gameServer->running())
+        {
+            sharedData.serverThread->launch();
+
+            sf::Clock clock;
+            while (!sharedData.gameServer->getSteamID().IsValid()
+                && clock.getElapsedTime().asSeconds() < 3.f) {
+            }
+
+            auto newMsg = xy::App::getActiveInstance()->getMessageBus().post<SystemEvent>(MessageID::SystemMessage);
+
+            if (sharedData.gameServer->getSteamID().IsValid())
+            {
+                sharedData.serverID = sharedData.gameServer->getSteamID();
+                xy::Logger::log("Created server instance " + std::to_string(sharedData.serverID.ConvertToUint64()), xy::Logger::Type::Info);
+                newMsg->action = SystemEvent::ServerStarted;
+            }
+            else
+            {
+                sharedData.serverID = {};
+                sharedData.gameServer->stop();
+                xy::Logger::log("Failed creating game server instance", xy::Logger::Type::Error);
+                newMsg->action = SystemEvent::ServerStopped;
+            }
+        }
+        else if (data.action == SystemEvent::RequestStopServer)
+        {
+            sharedData.gameServer->stop();
+            sharedData.serverThread->wait();
+        }
+    }
+}
